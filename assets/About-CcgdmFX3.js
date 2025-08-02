e48945da2bd33bf2300669fe74acaab9fa3722c6
Alexander <alwong@ya.ru>
2025-08-03 01:34:23 +0400

    Updates

---
+++ assets/About-CcgdmFX3.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-C9FK6NPg.js";import{A as o}from"./AboutSection-COTOb8AF.js";function e(){return t.jsx("div",{className:"page",children:t.jsx(o,{})})}export{e as default};
---
+++ assets/Services-Af1hS98Y.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-C9FK6NPg.js";import{S as r}from"./ServicesSection-CZldRiqs.js";function t(){return e.jsx("div",{className:"page",children:e.jsx(r,{})})}export{t as default};
